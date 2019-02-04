b222d4f2066f9fbec09660630e87bc4b430a5b94
adan2 <ad8ndi@gmail.com>
2019-02-04 15:02:12 -0800

    New build.

---
+++ docs/oxygen-webhelp/app/nav-links/json/user-guide-applications-page-d66e209.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Resource Metrics ","href":"topics\/user-guide-resource-metrics.html","attributes": {"data-id":"user-guide-resource-metrics",},"menu": {"hasChildren":false,},"tocID":"user-guide-resource-metrics-d66e217","topics":[]}]});
